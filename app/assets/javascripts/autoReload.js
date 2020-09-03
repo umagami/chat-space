9703f17870ea85dddaf346beebdf522fe8542667
umagami <uma73gami@gmail.com>
2020-09-03 09:58:09 +0900

    make 2

--- app/assets/javascripts/autoReload.js
+++ app/assets/javascripts/autoReload.js
@@ -2,7 +2,7 @@ $(function(){
   function buildHTML(message){
     if ( message.image ) {
       let html =
-        `<div class="Chat-main__message-list__box-ru">
+        `<div class="Chat-main__message-list__box-ru" data-message-id=${message.id}>
           <div class="Chat-main__message-list__box-ru__flex">
             <div class="Chat-main__message-list__box-ru__messages">
               ${message.user_name}
@@ -19,7 +19,7 @@ $(function(){
       return html;
     } else {
       let html =
-      `<div class="Chat-main__message-list__box-ru">
+      `<div class="Chat-main__message-list__box-ru" data-message-id=${message.id}>
         <div class="Chat-main__message-list__box-ru__flex">
           <div class="Chat-main__message-list__box-ru__messages">
             ${message.user_name}
@@ -28,37 +28,43 @@ $(function(){
             ${message.created_at}
           </div>
         </div>
-        <div class="Message">
-          <p class="Chat-main__message-list__box-ru__messages__days__content">
-            ${message.content}
-          </p>
+        <div class="Chat-main__message-list__box-ru__messages__days__content">
+          ${message.content}
         </div>
       </div>`
       return html;
     };
   }
 
-  $('.Chat-main__message-form').on('submit', function(e){
-    e.preventDefault();
-    let formData = new FormData(this);
-    let url = $(this).attr('action');
+  let reloadMessages = function() {
+
+    let last_message_id = $('.Chat-main__message-list__box-ru:last').data("message-id") || 0;
     $.ajax({
-      url: url,
-      type: "POST",
-      data: formData,
+
+      url: "api/messages",
+
+      type: 'get',
       dataType: 'json',
-      processData: false,
-      contentType: false
+
+      data: {id: last_message_id}
     })
-    .done(function(data){
-      let html = buildHTML(data);
-      $('.Chat-main__message-list').append(html);      
-      $('.Chat-main__message-list').animate({ scrollTop: $('.Chat-main__message-list')[0].scrollHeight});
-      $('form')[0].reset();
-      $('.Chat-main__message-form__box-bun__message__send').prop('disabled', false)
+    .done(function(messages) {
+      console.log(messages)
+      if (messages.length !== 0) {
+
+        let insertHTML = '';
+
+        $.each(messages, function(i, message) {
+          insertHTML += buildHTML(message)
+        });
+
+        $('.Chat-main__message-list').append(insertHTML);
+        $('.Chat-main__message-list').animate({ scrollTop: $('.Chat-main__message-list')[0].scrollHeight});
+      }
     })
     .fail(function() {
-      alert("メッセージ送信に失敗しました");
+      alert('error');
     });
-  });
+  };
+  setInterval(reloadMessages, 7000);
 });
